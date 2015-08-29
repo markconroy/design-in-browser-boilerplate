d14a0c07a66bbef9ec638dd4840aba759bcbe2e3
Mark Conroy <info@adesignforlife.net>
2015-08-29 14:27:49 +0100

    contact form items

--- contact/index.php
+++ contact/index.php
@@ -1,7 +1,12 @@
 <?php include('../includes/meta.php'); ?>
-<?php include('../includes/header.php'); ?>
-<div id="contact">
+<div class="page-wrapper">
+  <?php include('../includes/header.php'); ?>
   <?php include('../includes/nav.php'); ?>
+  <div id="contact">
+    <div class="inner-container">
+      <h2>Contact Us</h2>
+      <?php include ('main.php'); ?>
+    </div>
+  </div>
 </div>
-<?php include('main.php'); ?>
 <?php include('../includes/layouts/footer/footer.php'); ?>
