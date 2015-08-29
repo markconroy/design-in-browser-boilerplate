ff93dc5b1038e8d096b430062fc77a3836b4dc4f
Mark Conroy <info@adesignforlife.net>
2015-08-29 12:49:14 +0100

    remove black directory

--- _blank-directory/index.php
+++
@@ -1,7 +0,0 @@
-<?php include('../includes/meta.php'); ?>
-<?php include('../includes/header.php'); ?>
-<div id="why-eircom">
-  <?php include('../includes/nav.php'); ?>
-</div>
-<?php include('main.php'); ?>
-<?php include('../includes/footer.php'); ?>
--- _blank-directory/main.php
+++
@@ -1,7 +0,0 @@
-<section>
-  <div id="main" class="outer-container">
-    <div class="inner-container">
-      <p>Content</p>
-    </div>
-  </div>
-</section>
