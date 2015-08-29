e424d6b5cdc2bcb4b0a9563ca2c8aa31f20c5609
Mark Conroy <info@adesignforlife.net>
2015-08-29 14:28:20 +0100

    add images to news

--- news/news-styles/news-item-teaser.php
+++ news/news-styles/news-item-teaser.php
@@ -1,5 +1,12 @@
-<div class="node-teaser news-item">
-  <h2>News title</h2>
-  <p>This is a news teaser, consectetur adipiscing elit. Mauris ultrices hendrerit mauris. In gravida nisl erat, vel pellentesque erat molestie ac. Donec convallis neque neque, a efficitur nunc tempus sed. Cras a posuere purus. Suspendisse et bibendum dui. Fusce varius a quam in vulputate. Nullam eget nisi sed neque feugiat pretium. Fusce porttitor leo feugiat lacinia cursus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc varius accumsan urna eu tristique. Nulla et bibendum mi. Nunc non sollicitudin sem, ac pharetra metus. Curabitur vel ligula volutpat, vehicula mi nec, blandit metus.</p>
-  <a href="/news/news-item" class="node-readmore">Read more</a>
+<div class="node-type-news">
+    <div class="node-teaser news-item">
+    <h2>News title</h2>
+    <div class="field-name-field-main-image">
+      <img src="http://lorempixel.com/400/200/">
+    </div>
+    <div class="field-name-body">
+      <p>This is a news teaser, consectetur adipiscing elit. Mauris ultrices hendrerit mauris. In gravida nisl erat, vel pellentesque erat molestie ac. Donec convallis neque neque, a efficitur nunc tempus sed. Cras a posuere purus. Suspendisse et bibendum dui. Fusce varius a quam in vulputate. Nullam eget nisi sed neque feugiat pretium. Fusce porttitor leo feugiat lacinia cursus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc varius accumsan urna eu tristique. Nulla et bibendum mi. Nunc non sollicitudin sem, ac pharetra metus. Curabitur vel ligula volutpat, vehicula mi nec, blandit metus.This is a news teaser, consectetur adipiscing elit. Mauris ultrices hendrerit mauris. In gravida nisl erat, vel pellentesque erat molestie ac. Donec convallis neque neque, a efficitur nunc tempus sed. Cras a posuere purus. Suspendisse et bibendum dui. Fusce varius a quam in vulputate. Nullam eget nisi sed neque feugiat pretium. Fusce porttitor leo feugiat lacinia cursus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc varius accumsan urna eu tristique. Nulla et bibendum mi. Nunc non sollicitudin sem, ac pharetra metus. Curabitur vel ligula volutpat, vehicula mi nec, blandit metus.</p>
+      <a href="/news/news-item" class="node-readmore">Read more</a>
+    </div>
+  </div>
 </div>
